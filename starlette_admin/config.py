42dd3228c6530dfd1aa524f887327691a66cb939
Stuart George <stuart@accentdesign.co.uk>
2019-06-12 10:06:49 +0100

    Fix travis

--- starlette_admin/config.py
+++ starlette_admin/config.py
@@ -4,7 +4,9 @@ from starlette_core.templating import Jinja2Templates
 
 class AppConfig:
     logout_url = ""
-    templates: Jinja2Templates = Jinja2Templates(loader=jinja2.PackageLoader("starlette_admin"))
+    templates: Jinja2Templates = Jinja2Templates(
+        loader=jinja2.PackageLoader("starlette_admin")
+    )
 
 
 config = AppConfig()
